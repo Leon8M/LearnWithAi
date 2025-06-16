ae57f5c9024062e237aed7ff2ffa4cf0f9a47862
Leon Munene <leonmunene@gmail.com>
2025-06-16 15:30:17 +0300

    Setting up Gemini API, updating schema to store course details

---
+++ app/api/generate-course-layout/route.jsx
@@ -0,0 +1,3 @@
+export async function POST(req) {
+    const formData = await req.json();
+}
--- config/schema.js
+++ config/schema.js
@@ -1,3 +1,4 @@
+import { boolean, json } from "drizzle-orm/gel-core";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
@@ -5,4 +6,17 @@ export const usersTable = pgTable("users", {
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   subID: varchar()
+});
+
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar().notNull(),
+  name: varchar().notNull(),
+  description: varchar(),
+  chapters: integer().notNull(),
+  includeVideo: boolean().default(false),
+  difficulty: varchar().notNull(),
+  category: varchar(),
+  courseJson: json(),
+  userEmail: varchar('user_email').references(() => usersTable.email),
 });
